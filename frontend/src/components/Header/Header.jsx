b27654364000fd4b9b60cb09b420ae5d06369fcb
Kartik mohod <130827601+Kartikmohod123@users.noreply.github.com>
2025-07-06 16:37:43 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import './Header.css'
+import React, { useState } from 'react';
+import './Header.css';
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <div className='header-contents'>
-                <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-                <button>View Menu</button>
-            </div>
-        </div>
-    )
-}
+  const [menu, setMenu] = useState("");
 
-export default Header
+  const handleViewMenu = () => {
+    setMenu("menu");
+    const section = document.getElementById("explore-menu");
+    section?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Order your favourite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes crafted
+          with the finest ingredients and culinary expertise. Our mission is to satisfy
+          your cravings and elevate your dining experience, one delicious meal at a time.
+        </p>
+        
+        <button
+          onClick={handleViewMenu}
+          className={menu === "menu" ? "active" : ""}
+        >
+          View Menu
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
